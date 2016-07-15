f0ebf9ba7d5b380ec2773b25bb70a3a1148b69c9
surya <surya@surya.com>
2016-07-15 12:34:56 +0530

    Update

--- js/mac-like-navigation.js
+++ js/mac-like-navigation.js
@@ -1,8 +1,18 @@
-(function($){
-	this.maclikeNav = function(id,arguments){
-		console.log(id+'   :   ' +arguments);
+(function(){
+	this.maclikeNav = function(id,data,arguments){
+		//console.log(id+'   :   ' +arguments);
+		$('#'+id).addClass('macnav');
+
+		$('.macnav').append('<ul class="macnav-list"></ul>');
+
+		$.each(data,function(index,object){
+			if(object.icon === 'font-awesome'){
+				$('.macnav-list').append('<li class="macnav-list-item fa fa-'+object.icontype+'"></li>');
+			}
+		})
+
 	}
 	function extendDefaults(arguments){
-		
+
 	}
 }());
