ce3c974c0ec46d65aae99c09e318a7d6995ec5ea
surya <surya@surya.com>
2016-07-20 19:13:43 +0530

    Update to UI and func

--- js/mac-like-navigation.js
+++ js/mac-like-navigation.js
@@ -1,13 +1,20 @@
 (function(){
 	this.maclikeNav = function(id,data,arguments){
 		//console.log(id+'   :   ' +arguments);
+
+
+
 		$('#'+id).addClass('macnav');
 
 		$('.macnav').append('<ul class="macnav-list"></ul>');
 
+		//setting the width
+
+		$('.macnav').css('width',data.length * 6+'%');
+
 		$.each(data,function(index,object){
 			if(object.icon === 'font-awesome'){
-				$('.macnav-list').append('<li class="macnav-list-item fa fa-'+object.icontype+'"></li>');
+				$('.macnav-list').append('<li class="macnav-list-item "><span class="fa fa-'+object.icontype+'"></span></li>');
 			}
 		})
 
