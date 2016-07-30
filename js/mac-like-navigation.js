1f76f2423917357e82ac4a48024fdf9ab45bc127
Surya <iamssurya@gmail.com>
2016-07-30 12:45:14 +0530

    Design update

--- js/mac-like-navigation.js
+++ js/mac-like-navigation.js
@@ -9,13 +9,17 @@
 		$('.macnav').append('<ul class="macnav-list"></ul>');
 
 		//setting the width
-
-		$('.macnav').css('width',data.length * 6+'%');
+		//$('.macnav').css('min-width',data.length * 6+'%');
+		//$('.macnav').css('max-width',data.length * 6.5+'%');
 
 		$.each(data,function(index,object){
 			if(object.icon === 'font-awesome'){
-				$('.macnav-list').append('<li class="macnav-list-item "><span class="fa fa-'+object.icontype+'"></span></li>');
+				$('.macnav-list').append('<li class="macnav-list-item "><span class="icon_'+index+' fa fa-'+object.icontype+'"></span></li>');
+				if(object.color){
+					$('.icon_'+index).css('color',object.color);
+				}
 			}
+
 		})
 
 	}
